fd110ef9ea499e8b49e5aa6e0a613386e0c8711f
Christian Haudum <christian.haudum@gmail.com>
2023-09-08 13:23:22 +0200

    Remove inline Store interface from LokiStore struct
    Use composition instead of "inline inheritance". This requires LokiStore
    to implement additional methods that were previously inherited from the
    inline Store field.
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/storage/store_test.go
+++ pkg/storage/store_test.go
@@ -488,7 +488,7 @@ func Test_store_SelectLogs(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &LokiStore{
-				Store: storeFixture,
+				store: storeFixture,
 				cfg: Config{
 					MaxChunkBatchSize: 10,
 				},
@@ -812,7 +812,7 @@ func Test_store_SelectSample(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &LokiStore{
-				Store: storeFixture,
+				store: storeFixture,
 				cfg: Config{
 					MaxChunkBatchSize: 10,
 				},
@@ -848,7 +848,7 @@ func (f fakeChunkFilterer) ShouldFilter(metric labels.Labels) bool {
 
 func Test_ChunkFilterer(t *testing.T) {
 	s := &LokiStore{
-		Store: storeFixture,
+		store: storeFixture,
 		cfg: Config{
 			MaxChunkBatchSize: 10,
 		},
@@ -938,7 +938,7 @@ func Test_store_GetSeries(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := &LokiStore{
-				Store: newMockChunkStore(chunkfmt, headfmt, streamsFixture),
+				store: newMockChunkStore(chunkfmt, headfmt, streamsFixture),
 				cfg: Config{
 					MaxChunkBatchSize: tt.batchSize,
 				},
@@ -1372,7 +1372,7 @@ func Test_OverlappingChunks(t *testing.T) {
 		}),
 	}
 	s := &LokiStore{
-		Store: &mockChunkStore{chunks: chunks, client: &mockChunkStoreClient{chunks: chunks}},
+		store: &mockChunkStore{chunks: chunks, client: &mockChunkStoreClient{chunks: chunks}},
 		cfg: Config{
 			MaxChunkBatchSize: 10,
 		},
@@ -1414,7 +1414,7 @@ func Test_GetSeries(t *testing.T) {
 
 	var (
 		store = &LokiStore{
-			Store: newMockChunkStore(chunkfmt, headfmt, []*logproto.Stream{
+			store: newMockChunkStore(chunkfmt, headfmt, []*logproto.Stream{
 				{
 					Labels: `{foo="bar",buzz="boo"}`,
 					Entries: []logproto.Entry{
